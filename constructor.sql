531ecc95db9e487bb06d402bd0b6571e6d073ed1
Guyrlam <guyrlamsilvaduque@gmail.com>
2022-11-15 16:32:59 -0300

    fix: update constructor.sql

---
+++ constructor.sql
@@ -0,0 +1,31 @@
+--- Criação do Banco de Dados
+CREATE DATABASE API_Typescript;
+
+---Criação das tabelas no Schema public
+CREATE TABLE public.Users (
+	id uuid NOT NULL PRIMARY KEY,
+	user_name varchar(80) NOT NULL,
+	email varchar(80) NOT NULL,
+	first_name varchar(80) NOT NULL,
+	last_name varchar(80) NOT NULL,
+	password varchar(80) NOT NULL,
+    squad uuid NOT NULL,
+    is_admin BOOLEAN NOT NULL,
+	created_at timestamptz default now(),
+	updated_at timestamptz,
+	deleted_at timestamptz
+);
+
+CREATE TABLE public.Squad (
+	id uuid NOT NULL PRIMARY KEY,
+	name varchar(80) NOT NULL,
+    leader uuid NOT NULL,
+	created_at timestamptz default now(),
+	updated_at timestamptz,
+	deleted_at timestamptz
+);
+
+
+ALTER TABLE User ADD CONSTRAINT User_fk0 FOREIGN KEY (squad) REFERENCES Squad(id);
+
+ALTER TABLE Squad ADD CONSTRAINT Squad_fk0 FOREIGN KEY (leader) REFERENCES Users(id);
