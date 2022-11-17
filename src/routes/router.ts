a6a9d39b80135ce4299128eb96c28135508d06b6
victorraffaele <victorraffaele@gmail.com>
2022-11-17 01:58:56 -0300

    feat: get and update user controller

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,14 +1,19 @@
 import { Router } from 'express';
-import { register, returnUsersList } from '../controllers/users';
+import {
+    register,
+    returnUsersList,
+    getUserId,
+    updateUser,
+} from '../controllers/users';
 import { verifyAdmToken } from '../middleware/login';
 
 const route = Router();
 
 route.post('/users/', register);
 route.get('/users', verifyAdmToken, returnUsersList);
-route.get('/users/:user_id');
+route.get('/users/:user_id', getUserId);
 route.get('/users/me');
-route.patch('/users/:user_id');
+route.patch('/users/:user_id', updateUser);
 
 route.post('/team/', register);
 route.get('/team/:team_id');
